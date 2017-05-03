9e7c80c57127c1b7ab52cf00023a686d48eddb47
jukim1234 <jukim1234@korea.com>
2017-05-03 18:30:23 +0900

    first commit

---
+++ program.js
@@ -0,0 +1,53 @@
+document.writeln('Hello, world! OOP Game<BR>');
+
+
+let person = new Object();
+person.name = "JW";
+person.cook = function(menu) {
+  this.state = "cooking";
+  this.menu = "";
+  this.menu = menu;
+}
+
+person.printState = function(which) {
+  document.writeln('======================');	
+  document.writeln('[State]'+ nvl(this.state));
+  if("menu" === which) {
+	 document.writeln('['+which+']'+this.menu); 
+  }
+}
+
+function nvl(val) {
+  return (val === undefined ? "":val);
+}
+
+
+let dummy = {
+    state : "멍하다...created via object literal",
+    menu : ["라면", "짜파게티"],
+    printState : function(){
+		let menus = "";
+		for(i in this.menu){ menus += this.menu[i] + ", "; } 
+		alert("몰라" + this.state + "--"+ menus); 
+	}
+};
+
+
+
+person.printState();
+
+person.cook("fried rice");
+person.printState("menu");
+
+dummy.printState();
+
+
+
+
+
+
+
+
+
+
+
